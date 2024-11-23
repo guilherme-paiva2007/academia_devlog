877a32da10c9cb8a8031ca7539f6c051b912483a
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2024-11-23 15:15:56 -0300

    tudo

---
+++ html/footer.php
@@ -0,0 +1,63 @@
+<footer>
+            <div id="footer">
+        
+                <div class="contato">
+                    <h2>Informações de Contato</h2>
+                    <p>Endereço: Av. Alegria, 100, Caçapava - SP, 12745-160</p>
+                    <p>Telefone: (12) 3653-1943</p>
+                    <p>E-mail: contato@sesi-senai.com.br</p>
+            
+                </div> 
+            
+                <div class="equipe">
+                    <h2>Equipe Desenvolvedora</h2>
+                        <p>Ana Lívia</p>
+                        <p>Gabriel Reis</p>
+                        <p>Guilherme Paiva</p>
+                </div>
+                </div>
+            </div>
+        </footer> 
+        <style>
+/* footer */
+footer {
+    background-color:rgba(99, 149, 187, 1);
+    width: 100%;
+    color: white;
+    margin-top: 60px;
+}
+
+#footer {
+    justify-content: space-evenly;
+    display: flex;
+    margin-top: 30px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+}
+
+footer h2 {
+    margin-top: 20px;
+    margin-bottom: 10px;
+    font-family: 'Montserrat', sans-serif;
+}
+
+footer p, footer a {
+    font-size: 14px;
+    line-height: 1.5;
+    font-family: "Plus Jakarta Sans", sans-serif;
+    padding: 3px;
+}
+
+footer a {
+    text-decoration: none;
+    color: #092c4f;
+}
+
+footer a:hover {
+    text-decoration: underline;
+}
+
+.contato {
+    margin-top: 5px;
+}
+        </style>
--- instrutor_aluno_cadastrar.php
+++ instrutor_aluno_cadastrar.php
@@ -1,5 +1,5 @@
 <?php
-include './php/page_config.php';
+// include './php/page_config.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -8,32 +8,117 @@ include './php/page_config.php';
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Registrar Aluno</title>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <?php include './html/head.php' ?>
+        <?php #include './html/head.php' ?>
     </head>
-    <body class="center">
-        <div>
-            <h2>Registrar Aluno</h2>
+    <?php
+    include './html/header.php';
+    ?>
+    <main class="main-content">
+    <section id="secao1">
+        <div id="box-login">
+            <div id="box-img-login">
+                <img id="img-login" src="img/logo2.png" alt="">
+            </div>
             <form method="post" id="form-register">
-                <label for="nome">Nome:</label>
-                <input type="text" id="nome" name="nome" required><br><br>
-    
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required><br><br>
-    
-                <label for="telefone">Telefone:</label>
-                <input type="tel" id="telefone" name="telefone" required><br><br>
-    
-                <label for="data_nascimento">Data de Nascimento:</label>
-                <input type="date" id="data_nascimento" name="data_nascimento" required><br><br>
-    
-                <label for="senha">Senha:</label>
-                <input type="password" id="senha" name="senha" required><br><br>
-    
-                <label for="confirmar_senha">Confirmar Senha:</label>
-                <input type="password" id="confirmar_senha" name="confirmar_senha" required><br><br>
-    
-                <button type="submit">Registrar</button>
+                <h1>Registrar Aluno</h1>
+                <input id="email" class="inserir" name="email" type="email" placeholder="Email" required>
+                <input id="senha" class="inserir" name="senha" type="password" placeholder="Senha" required>
+                <a href="../hostcenter/index.html"><input id="entrar" type="submit" value="Entrar"></a>
+                <p class="outro">Não possui uma conta? <a href="cadastro.php">Cadastre-se!</a></p>
             </form>
         </div>
+    </section>
+
+   <?php
+   include './html/footer.php';
+   ?> 
+</main>
+</body>
+
+
+        
     </body>
 </html>
+<style>
+    #secao1{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 90vh;
+    width: 100%;
+    background-color: #2D4455;
+}
+
+#box-login{
+    display: flex;
+    background-color: white;
+    width: 720px;
+    border-radius: 20px;
+}
+
+#box-img-login{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #dde8f1;
+    width: 350px;
+    height: 450px;
+    border-top-left-radius: 20px;
+    border-bottom-left-radius: 20px;
+}
+
+#img-login{
+    width: 335px;
+    height: 280px;
+}
+
+form{
+    width: 360px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+h1{
+    font-family: Reem Kufi;
+    font-size: 40px;
+    color: #00304D;
+    margin-bottom: 30px;
+}
+
+
+.inserir{
+    width: 230px;
+    height: 35px;
+    border: none;
+    border-radius: 50px;
+    background-color: rgb(227, 227, 227);
+    margin-bottom: 10px;
+    padding-left: 16px;
+    font-family: Inter;
+}
+
+#entrar{
+    background-color: #75B0DD;
+    color: white;
+    border: none;
+    border-radius: 10px;
+    width: 200px;
+    height: 40px;
+    cursor: pointer;
+    margin: 10px;
+    font-family: Reem Kufi;
+    font-size: 15px;
+    transition: background-color 0.2s;
+}
+
+#entrar:hover{
+    background-color: #92c3e8;
+}
+
+.outro{
+    text-align: center;
+    margin-top: 40px;
+}
+</style>
